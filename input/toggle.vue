0b9312a576dd43dfbde5c8dc28908d7d46c6f47e
Stoo <stoo.goff@gmail.comn>
2024-02-22 23:25:02 +0000

    Added toggle

---
+++ input/toggle.vue
@@ -0,0 +1,58 @@
+<template>
+	<input
+		type="checkbox"
+		class="checkbox"
+		:class="classes"
+		:checked="value"
+		:name="name"
+		:disabled="disabled"
+		@click="toggle"
+	/>
+</template>
+<script>
+
+import Vue from 'vue'
+import { createComponent } from '../props'
+
+const CLASS_LIST = [
+	// colours
+	'toggle-primary',
+	'toggle-secondary',
+	'toggle-accent',
+	// states
+	'toggle-info',
+	'toggle-success',
+	'toggle-warning',
+	'toggle-error',
+	// sizes
+	'toggle-xs',
+	'toggle-sm',
+	'toggle-md',
+	'toggle-lg',
+]
+
+const component = createComponent('toggle', CLASS_LIST)
+
+component.props.name = {
+	type: String,
+	default: '',
+}
+component.props.value = {
+	type: Boolean,
+	default: false,
+}
+component.props.disabled = {
+	type: Boolean,
+	default: false,
+}
+component.methods = {
+	toggle() {
+		if(this.disabled) return
+
+		this.$emit('input', !this.value)
+	}
+}
+
+export default Vue.component('Toggle', component)
+
+</script>
