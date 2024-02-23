0af9eba254efcd51aa112917ee0d2cca1437eeb0
Stoo <stoo.goff@gmail.comn>
2024-02-23 00:28:11 +0000

    Icon and base dropdown

---
+++ actions/dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="dropdown dropdown-left dropdown-end">
+		<div tabindex="0" role="button" class="btn m-1">Click</div>
+		<ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+			<li><a>Item 1</a></li>
+			<li><a>Item 2</a></li>
+		</ul>
+	</div>
+</template>
+<script>
+
+import Vue from 'vue'
+import { createComponent } from '../props'
+
+const CLASS_LIST = [
+	// position
+	'dropdown-end',
+	'dropdown-top',
+	'dropdown-bottom',
+	'dropdown-left',
+	'dropdown-right',
+	// state
+	'dropdown-hover',
+	'dropdown-open',
+]
+
+export default Vue.component('Dropdown', createComponent('dropdown', CLASS_LIST))
+
+</script>
---
+++ feedback/icon.vue
@@ -0,0 +1,17 @@
+<template>
+	<span class="inline-block w-5" v-html="icon" />	
+</template>
+<script>
+
+import Vue from 'vue'
+
+export default Vue.component('Icon', {
+	props: {
+		icon: {
+			type: String,
+			required: true,
+		},
+	},
+})
+
+</script>
