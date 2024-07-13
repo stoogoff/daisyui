9b19121291d376ce2643a09f8ff6396e7a023633
Stoo <stoo.goff@gmail.comn>
2024-07-13 10:45:03 +0100

    Added more helper functions and storage class

--- utils/string.js
+++ utils/string.js
@@ -12,3 +12,21 @@ export const createId = (length = 6) => {
 
 	return output.join('')
 }
+
+export const toTitleCase = text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+
+export const slugify = text => text
+	.toLowerCase()
+	.trim()
+	.replace(/^\W+/, '')
+	.replace(/\s+/g, '-')
+
+export const normalise = text => 
+	text.toString()
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.toLowerCase()
+
+export const search = (input, text) => normalise(input).indexOf(normalise(text)) !== -1
+
+export const searchObject = (input, text) => Object.values(input).some(value => search(value, text))
