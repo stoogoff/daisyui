23064482d4efa469b568d40a4aeb52ba2b934c25
Stoo <stoo.goff@gmail.comn>
2024-07-13 10:44:20 +0100

    Added sizes to icon

--- feedback/icon.vue
+++ feedback/icon.vue
@@ -1,5 +1,5 @@
 <template>
-	<span class="inline-block w-5 h-5" v-html="icon" />	
+	<span class="inline-block" :class="size" v-html="icon" />	
 </template>
 <script>
 
@@ -10,6 +10,28 @@ export default Vue.component('Icon', {
 		icon: {
 			type: String,
 			required: true,
+		},
+		sm: {
+			type: Boolean,
+			default: false,
+		},
+		lg: {
+			type: Boolean,
+			default: false,
+		},
+		xLg: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	computed: {
+		size() {
+			if(this.sm) return 'w-3 h-3'
+			if(this.lg) return 'w-7 h-7'
+			if(this.xLg) return 'w-10 h-10'
+
+			return 'w-5 h-5'
 		},
 	},
 })
